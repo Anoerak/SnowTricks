076a145bac9d42c00f0e2e28ebe70b813853e60e
Anoerak <seb@iamseb.dev>
2023-03-14 07:39:24 +0100

    Load 8 more tricks if available on click and will remove the Load More button if no more tricks are avavailable in the database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,20 +13,24 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(TrickRepository $repo): Response
     {
-        // We get the first 10 Tricks
+        // We get the first 8 Tricks
         $tricks = $repo->findBy([], ['id' => 'ASC'], 8, 0);
+
+        // We get how many rows are in this table
+        $tricksCount = $repo->count([]);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'tricks' => $tricks,
+            'tricksCount' => $tricksCount,
         ]);
     }
 
-    #[Route('/load-more-tricks', name: 'app_load_more_tricks')]
-    public function loadMoreTricks(TrickRepository $repo): Response
+    #[Route('/load-more-tricks/{lastTrickIndex}', name: 'app_load_more_tricks')]
+    public function loadMoreTricks(TrickRepository $repo, $lastTrickIndex): Response
     {
-        // We get the next 10 Tricks
-        $tricks = $repo->findBy([], ['id' => 'ASC'], 9, 8);
+        // We get the next 9 Tricks
+        $tricks = $repo->findBy([], ['id' => 'ASC'], 9, $lastTrickIndex);
 
         return $this->render('home/display_tricks/more.html.twig', [
             'controller_name' => 'HomeController',
--- templates/home/display_tricks/display_tricks.js
+++ templates/home/display_tricks/display_tricks.js
@@ -1,19 +1,20 @@
 const displayMoreButton = document.getElementById('display-more_button');
-
 displayMoreButton.addEventListener('click', displayMoreTricks);
 
 function displayMoreTricks() {
-	getMoreTricks();
+	getMoreTricks(getLastTrickNumber());
 }
 
-// window.onscroll = function (ev) {
-// 	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 370) {
-// 		getMoreTricks();
-// 	}
-// };
+// We get the number of tricks displayed on screen
+function getLastTrickNumber() {
+	let tricksArray = document.getElementsByClassName('trick');
+	let lastTrickNumber = parseInt(tricksArray.length);
+	return lastTrickNumber;
+}
 
-let getMoreTricks = function () {
-	let url = 'https://127.0.0.1:8000/load-more-tricks';
+// We query for more tricks
+function getMoreTricks(lastTrickIndex) {
+	let url = 'https://127.0.0.1:8000/load-more-tricks/' + lastTrickIndex;
 	fetch(url)
 		.then(function (response) {
 			return response.text();
@@ -25,10 +26,15 @@ let getMoreTricks = function () {
 			tricks.removeChild(loadMoreButton);
 			// We insert the new tricks before the load more button in the tricks list
 			tricks.insertAdjacentHTML('beforeend', html);
-			// We insert the load more button at the end of the tricks list
-			tricks.insertAdjacentHTML('beforeend', loadMoreButton.outerHTML);
+			// We check if there an id 'tricks_count_' + lastTrickIndex in the tricks list
+			// If there is no id 'tricks_count_' + lastTrickIndex, it means that there is more tricks to display
+			// So we add the load more button
+			if (!document.getElementById('tricks_count_' + getLastTrickNumber())) {
+				// We insert the load more button at the end of the tricks list
+				tricks.insertAdjacentHTML('beforeend', loadMoreButton.outerHTML);
+			}
 		})
 		.catch(function (error) {
 			console.log(error);
 		});
-};
+}
